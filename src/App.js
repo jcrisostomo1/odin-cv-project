cf9ede0349cb1568a08a0b30ee8f6c0836784ae4
Josh Crisostomo <joshuacr22@gmail.com>
2022-07-19 13:47:31 -0400

    add starter files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import './styles/App.css' 
+import 'font-awesome/css/font-awesome.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>CV Application</h1>
+        </header>
+        <main>
+         
+        </main>
+        <footer>
+          <a href="https://github.com/jcrisostomo1" target="_blank"><i className="fa fa-github fa-lg"></i></a>
+          <p>Made by: jcrisostomo1</p>
+      </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
