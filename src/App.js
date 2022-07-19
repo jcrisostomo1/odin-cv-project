9a3eea808ba266ae29859b8b69aa4855c03f108b
Josh Crisostomo <joshuacr22@gmail.com>
2022-07-19 15:14:13 -0400

    add general info section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from "react";
+import General from "./components/General";
 import './styles/App.css' 
 import 'font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
-
   constructor() {
     super();
-
   }
 
   render() {
@@ -16,7 +15,7 @@ class App extends Component {
           <h1>CV Application</h1>
         </header>
         <main>
-         
+        <General/>
         </main>
         <footer>
           <a href="https://github.com/jcrisostomo1" target="_blank"><i className="fa fa-github fa-lg"></i></a>
--- src/components/General.js
+++ src/components/General.js
@@ -1,1 +1,24 @@
+import React, {Component} from "react";
+import '../styles/General.css'
 
+class General extends Component {
+	constructor() {
+      super();
+  }
+
+  render() {
+    return (
+      <div>
+				<form onSubmit>
+          <label htmlFor="taskInput">First Name: </label>
+          <input type="text" id="taskInput"/>
+					<button>Edit</button>
+					<label htmlFor="taskInput">First Name: </label>
+          <input type="text" id="taskInput"/>
+					<button>Edit</button>
+        </form>
+      </div>
+    );
+	}
+}
+export default General;
