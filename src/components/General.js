5a4de6c2d7aea97dd9b824a1c5f001c151ced9a1
Josh Crisostomo <joshuacr22@gmail.com>
2022-07-28 14:06:45 -0400

    finish styling general info

--- src/components/General.js
+++ src/components/General.js
@@ -45,22 +45,24 @@ class General extends Component {
 				<div className={generalStyles.form}>
           <div>
             {generalInfoSaved?<p><strong>First Name: </strong>{firstName}</p>
-            :<><label htmlFor="firstName">First Name: </label><input onChange={this.handleChange} value={firstName} type="text" id="firstName" placeholder="John" required/></>}
+            :<><label htmlFor="firstName">First Name:</label><input onChange={this.handleChange} value={firstName} type="text" id="firstName" placeholder="John" required/></>}
           </div> 
           <div>
             {generalInfoSaved?<p><strong>Last Name: </strong>{lastName}</p>
-            :<><label htmlFor="lastName">Last Name: </label><input onChange={this.handleChange} value={lastName} type="text" id="lastName" placeholder="Doe" required/></>}
+            :<><label htmlFor="lastName">Last Name:</label><input onChange={this.handleChange} value={lastName} type="text" id="lastName" placeholder="johndoe@test.com" required/></>}
           </div> 
           <div>
             {generalInfoSaved?<p><strong>Email: </strong>{email}</p>
-            :<><label htmlFor="email">Email: </label><input onChange={this.handleChange} value={email} type="email" id="email" placeholder="Email" required/></>}
+            :<><label htmlFor="email">Email:</label><input onChange={this.handleChange} value={email} type="email" id="email" placeholder="Email" required/></>}
           </div> 
           <div>
             {generalInfoSaved?<p><strong>Phone: </strong>{phone}</p>
-            :<><label htmlFor="phone">Phone: </label><input onChange={this.handleChange} value={phone} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"id="phone" placeholder="Phone" required/></>}
+            :<><label htmlFor="phone">Phone:</label><input onChange={this.handleChange} value={phone} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"id="phone" placeholder="123-456-7890" required/></>}
           </div> 
-          {generalInfoSaved?<button  className={generalStyles.button} onClick={this.onEditInfo}>Edit</button>:<button className={generalStyles.button} onClick={this.onSaveInfo}>Save</button>}
         </div>
+        <div className={generalStyles.button} >
+          {generalInfoSaved?<button onClick={this.onEditInfo}>Edit</button>:<button onClick={this.onSaveInfo}>Save</button>}
+          </div>
       </div>
       </>
     )
