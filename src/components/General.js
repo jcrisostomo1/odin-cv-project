f24df04c1e8c299f899dda66625b083954d32e75
Josh Crisostomo <joshuacr22@gmail.com>
2022-07-28 10:33:06 -0400

    add css styles

--- src/components/General.js
+++ src/components/General.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import generalStyles from "../styles/General.module.css"
 
 class General extends Component {
 	constructor(props) {
@@ -40,22 +41,26 @@ class General extends Component {
     return (
       <>
       <h3>{this.props.title}</h3>
-      <div>
-				<form>
+      <div className={generalStyles.main}>
+				<div className={generalStyles.form}>
           <div>
-            {generalInfoSaved?<p><strong>First Name: </strong>{firstName}</p>:<input onChange={this.handleChange} value={firstName} type="text" id="firstName" placeholder="First Name" required/>}
+            {generalInfoSaved?<p><strong>First Name: </strong>{firstName}</p>
+            :<><label htmlFor="firstName">First Name: </label><input onChange={this.handleChange} value={firstName} type="text" id="firstName" placeholder="John" required/></>}
           </div> 
           <div>
-            {generalInfoSaved?<p><strong>Last Name: </strong>{lastName}</p>:<input onChange={this.handleChange} value={lastName} type="text" id="lastName" placeholder="Last Name" required/>}
+            {generalInfoSaved?<p><strong>Last Name: </strong>{lastName}</p>
+            :<><label htmlFor="lastName">Last Name: </label><input onChange={this.handleChange} value={lastName} type="text" id="lastName" placeholder="Doe" required/></>}
           </div> 
           <div>
-            {generalInfoSaved?<p><strong>Email: </strong>{email}</p>:<input onChange={this.handleChange} value={email} type="email" id="email" placeholder="Email" required/>}
+            {generalInfoSaved?<p><strong>Email: </strong>{email}</p>
+            :<><label htmlFor="email">Email: </label><input onChange={this.handleChange} value={email} type="email" id="email" placeholder="Email" required/></>}
           </div> 
           <div>
-            {generalInfoSaved?<p><strong>Phone: </strong>{phone}</p>:<input onChange={this.handleChange} value={phone} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"id="phone" placeholder="Phone" required/>}
+            {generalInfoSaved?<p><strong>Phone: </strong>{phone}</p>
+            :<><label htmlFor="phone">Phone: </label><input onChange={this.handleChange} value={phone} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"id="phone" placeholder="Phone" required/></>}
           </div> 
-          {generalInfoSaved?<button onClick={this.onEditInfo}>Edit</button>:<button onClick={this.onSaveInfo}>Save</button>}
-        </form>
+          {generalInfoSaved?<button  className={generalStyles.button} onClick={this.onEditInfo}>Edit</button>:<button className={generalStyles.button} onClick={this.onSaveInfo}>Save</button>}
+        </div>
       </div>
       </>
     )
