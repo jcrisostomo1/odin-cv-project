9a27ad94be89a216ee8832eca4f12048852df01f
jcrisostomo1 <joshuacr22@gmail.com>
2022-07-28 22:17:16 -0400

    add css styling to experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import experienceStyles from '../styles/Experience.module.css'
 
 export default class Experience extends Component {
 
@@ -42,31 +43,34 @@ export default class Experience extends Component {
     const { id, experienceInfoSaved, companyName, positionTitle, jobDescription, exStart, exEnd} = this.state;
     
     return (
-      <>
-      <div className='main'>
-      <form>
-        <div>
-          {experienceInfoSaved?<p><strong>Company: </strong>{companyName}</p>:<input onChange={this.handleChange} value={companyName} type="text" id="companyName" placeholder="Company" required/>}
-        </div> 
-        <div>
-          {experienceInfoSaved?<p><strong>Position: </strong>{positionTitle}</p>:<input onChange={this.handleChange} value={positionTitle} type="text" id="positionTitle" placeholder="Position" required/>}
-        </div> 
-        <div>
-         {experienceInfoSaved?<p><strong>Job Description: </strong>{jobDescription}</p>:<textarea onChange={this.handleChange} value={jobDescription} id="jobDescription" placeholder="Job Description" required/>}
-        </div> 
-        <div>
-          {experienceInfoSaved?<p><strong>Start: </strong>{exStart}</p>:<><label htmlFor='exStart'>Start: </label>
-          <input onChange={this.handleChange} value={exStart} type="date" id="exStart" required/></>}
-        </div> 
-        <div>
-          {experienceInfoSaved?<p><strong>End: </strong>{exEnd}</p>:<><label htmlFor='exEnd'>End: </label>
-          <input onChange={this.handleChange} value={exEnd} type="date" id="exEnd" required/></>}
+      <div className={experienceStyles.main}>
+        <div className={experienceStyles.form}>
+          <div>
+            {experienceInfoSaved?<p><strong>Company: </strong>{companyName}</p>
+            :<><label htmlFor="companyName">Company Name:</label><input onChange={this.handleChange} value={companyName} type="text" id="companyName" placeholder="Company" required/></>}
+          </div> 
+          <div>
+            {experienceInfoSaved?<p><strong>Position: </strong>{positionTitle}</p>
+            :<><label htmlFor="position">Position:</label><input onChange={this.handleChange} value={positionTitle} type="text" id="positionTitle" placeholder="Position" required/></>}
+          </div> 
+          <div>
+            {experienceInfoSaved?<p><strong>Start: </strong>{exStart}</p>:<><label htmlFor='exStart'>Start: </label>
+            <input onChange={this.handleChange} value={exStart} type="date" id="exStart" required/></>}
+          </div> 
+          <div>
+            {experienceInfoSaved?<p><strong>End: </strong>{exEnd}</p>:<><label htmlFor='exEnd'>End: </label>
+            <input onChange={this.handleChange} value={exEnd} type="date" id="exEnd" required/></>}
+          </div>
         </div>
-        {experienceInfoSaved?<button onClick={this.onEditInfo}>Edit</button>:<button onClick={this.onSaveInfo}>Save</button> }
-        <button type="button" onClick={()=>this.props.handleDelete('experience', id)}>Delete</button>
-      </form>
+        <div className={experienceStyles.jobDescription}>
+          {experienceInfoSaved?<p><strong>Job Description: </strong>{jobDescription}</p>
+          :<><label htmlFor="jobDescription">Job Description:</label><textarea onChange={this.handleChange} value={jobDescription} id="jobDescription" placeholder="Job Description" required/></>}
+        </div> 
+        <div>
+          {experienceInfoSaved?<button onClick={this.onEditInfo}>Edit</button>:<button onClick={this.onSaveInfo}>Save</button> }
+          <button type="button" onClick={()=>this.props.handleDelete('experience', id)}>Delete</button>
+        </div>
       </div>
-      </>
     )}
 }
 
